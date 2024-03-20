c69aeb5b0735e63abba6b11c429fd759bae9d794
ved-thakre <vedantthakre7@gmail.com>
2024-03-20 21:29:21 +0530

    Created Address model

--- models/addressModel.js
+++ models/addressModel.js
@@ -1,5 +1,43 @@
 import { sequelize, DataTypes } from "sequelize";
 
 const Address = sequelize.define("Address", {
-    
-})
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  address1: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  address2: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  state: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  pincode: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+export default Address;
