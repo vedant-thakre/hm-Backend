7d2fe3d1387b58c0a5581e48e80767acbe20de07
ved-thakre <vedantthakre7@gmail.com>
2024-03-26 00:35:32 +0530

    Changes in Request API

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { SuperAdminAccess } from "../config/configfunctions.js";
-import { AcceptRequest, getAllRequest } from "../controllers/requestController.js";
+import { AcceptRequest, RejectRequest, getAllRequest } from "../controllers/requestController.js";
 
 const router = express.Router();
 
 router.put("/accept/:id", SuperAdminAccess, AcceptRequest);
+router.delete("/reject/:id", SuperAdminAccess, RejectRequest);
 router.get("/get-all-request", SuperAdminAccess, getAllRequest);
 
 export default router;
